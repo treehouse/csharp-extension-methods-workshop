f1d1c8b63a8c6f4bdf1de40aa1cbce85045d9d08
jeremym <jeremym@teamtreehouse.com>
2016-10-11 11:49:21 -0700

    Initial commit

---
+++ ExtensionMethodsDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Treehouse.ExtensionMethodsDemo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var synonymsForBest = new List<string>
+            {
+                "best",
+                "finest",
+                "greatest",
+                "top",
+                "foremost",
+                "leading",
+                "most excellent",
+                "preeminent",
+                "premier",
+                "chief",
+                "supreme",
+                "unrivaled",
+                "ultimate",
+                "perfect",
+                "incomparable",
+                "ideal"
+            };
+
+            Console.WriteLine($"My dog Jojo is the {GetRandomItem(synonymsForBest)} dog!");
+            Console.ReadLine();
+        }
+
+        private static string GetRandomItem(List<string> list)
+        {
+            var random = new Random();
+            return list[random.Next(0, list.Count)];
+        }
+    }
+}
